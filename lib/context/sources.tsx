e33a2d64cec6f862520c4362a111ef3d2d6d279d
geraud.henrion <geraud.henrion@nartex.fr>
2020-08-25 22:49:58 +0200

    add "map sources" features

--- lib/context/sources.tsx
+++ lib/context/sources.tsx
@@ -5,7 +5,14 @@ import { ContextKey, withContext, useContext, safeUseContext } from ".";
 
 export const sourcesKey: ContextKey<Sources> = Symbol("sources");
 
-export function provideSources<T>(sources: Sources, func: () => T) {
+export function provideSources<T>(
+  sources: Sources | ((sources: Sources) => Sources),
+  func: () => T
+) {
+  if (typeof sources === "function") {
+    return withContext(sourcesKey, sources(useSources()), func);
+  }
+
   return withContext(sourcesKey, sources, func);
 }
 
